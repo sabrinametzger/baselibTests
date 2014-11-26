d7b4c4041d2e93271e3fbfc416b5be5b60006fca
Sabrina Metzger <sabrina.metzger@1und1.de>
2014-11-26 16:10:02 +0100

    use 'nopt' now to parse arguments

--- setupWebdr.js
+++ setupWebdr.js
@@ -1,41 +1,32 @@
 /**
  *  example:
- *  mocha test.js --"http://<url>" --"http://<server>" --'{"browserName":"<browser>"}'
+ *  mocha test.js --url "http://<url>" --server http://<server>" --caps '{"browserName":"<browser>"}'
  *
  */
 var wd = require('webdriver-sync');
+var Args = require('arg-parser');
+var nopt = require('nopt');
+
 var webdr = module.exports = {};
 
 webdr.setup = function(wd) {
 
-  var caps;
-  var capabilities = [];
-  var url;
-  var server;
-  var args = process.argv;
-
-  for (var i = 0; i < args.length; i++) {
-    if (args[i].indexOf("--") === 0) {
-      args[i] = args[i].slice(2);
-    }
-  }
-  if (args[3].indexOf("no-timeout") == 0) {
-    url = args[4];
-    server = args[5];
-    caps = JSON.parse(args[6]);
-  } else {
-    url = args[3];
-    server = args[4];
-    caps = JSON.parse(args[5]);
-  }
+  knownOpts = {
+    "url": [String],
+    "server": [String],
+    "caps": [String]
+  }, parsed = nopt(knownOpts, process.argv, 0);
 
   var capability = new wd.DesiredCapabilities().firefox();
+  var caps = JSON.parse(parsed.caps);
   for (var key in caps) {
     capability.setCapability(key, caps[key]);
   }
-  driver = new wd.RemoteWebDriver(server + "/wd/hub", capability);
+  if (parsed.server) {
+    driver = new wd.RemoteWebDriver(parsed.server + "/wd/hub", caps);
+  }
   driver.manage().window().maximize();
-  driver.get(url);
+  driver.get(parsed.url);
   return driver;
 };
 
