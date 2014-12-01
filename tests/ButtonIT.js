6b6db24ad34306b0a768e268e1ea427bbd6a5c61
Sabrina Metzger <sabrina.metzger@1und1.de>
2014-12-01 14:44:47 +0100

    updated id of menu buttons

--- tests/ButtonIT.js
+++ tests/ButtonIT.js
@@ -17,7 +17,7 @@ describe("button widget", function() {
 
   it("click button", function() {
     //drawer has also id 'buttonWidget'
-    var menu = driver.findElements(wd.By.id("buttonWidget"));
+    var menu = driver.findElement(wd.By.id("buttonWidget"));
     webdrSetup.click(menu);
     wd.sleep(1000);
     var button = driver.findElement(wd.By.xpath("//button[contains(@id,'button-example')]"));
